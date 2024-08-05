e257ac6b45f69362c5ae781a06eb34e1a5aa92e1
Loris Puech <puech.loris@gmail.com>
2024-08-05 16:35:06 +0200

    update angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
@@ -12,22 +12,15 @@ import { DataService } from "./services/data.service";
 import { TechnologyComponent } from "./technology/technology.component";
 
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent,
-    DestinationComponent,
-    CrewComponent,
-    TechnologyComponent,
-    MainMenuComponent,
-    HomeComponent,
-  ],
-  providers: [DataService],
-  bootstrap: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        MainPageComponent,
+        DestinationComponent,
+        CrewComponent,
+        TechnologyComponent,
+        MainMenuComponent,
+        HomeComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [DataService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
